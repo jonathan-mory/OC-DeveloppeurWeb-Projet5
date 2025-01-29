6aa2d1a5cd112260a28515a029ca28bf66d43f09
Jonathan Mory <jomory@orange.fr>
2025-01-29 14:36:19 +0100

    Modification de la page d'accueil et création du style CSS

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,33 @@
+import Banner from '../../components/Banner'
+import Card from '../../components/Card'
+import HomeBanner from '../../assets/images/home_banner.jpg'
+import { useFetch } from '../../utils/hooks'
+import './Home.scss'
+
 function Home() {
-  return <h1>Chez vous, partout et ailleurs</h1>
+  const dataUrl = '/accomodations.json'
+  const { isLoading, data, error } = useFetch(dataUrl)
+  console.log(data)
+
+  if (isLoading) return <span>Chargement en cours</span>
+  if (error) return <span>Erreur lors du chargement des données</span>
+
+  return (
+    <main>
+      <Banner imageUrl={HomeBanner}>
+        <h1>Chez vous, partout et ailleurs</h1>
+      </Banner>
+      <section className="card-container">
+        {data.map((element) => (
+          <Card
+            key={element.id}
+            title={element.title}
+            coverUrl={element.cover}
+          />
+        ))}
+      </section>
+    </main>
+  )
 }
 
 export default Home
