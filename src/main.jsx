395ae89b94b33a0d94be3db428a1fe8aae51274d
Jonathan Mory <jomory@orange.fr>
2025-01-29 14:32:07 +0100

    Création des variables CSS et du style global

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './components/Router/index.jsx'
+import './main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
