ee1785bc9ace8d249bd041802fea8c9926ce5be0
Jonathan Mory <jomory@orange.fr>
2025-01-29 14:34:05 +0100

    Modification du composant Header et création de son style CSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,48 @@
 import { NavLink } from 'react-router'
+import Logo from '../../assets/images/logo_kasa.png'
+import './Header.scss'
+import { useEffect, useState } from 'react'
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 375)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 375)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <header>
-      <nav>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? 'active' : '')}
-        >
-          Accueil
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) => (isActive ? 'active' : '')}
-        >
-          A propos
-        </NavLink>
+    <header className="header">
+      <img src={Logo} alt="Logo de Kasa" className="header__logo" />
+      <nav className="header__navbar">
+        <ul className="navbar__list">
+          <li className="navbar__item">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `navbar__link ${isActive ? 'navbar__link--active' : ''}`
+              }
+            >
+              {isMobile ? 'ACCUEIL' : 'Accueil'}
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                `navbar__link ${isActive ? 'navbar__link--active' : ''}`
+              }
+            >
+              {isMobile ? 'A PROPOS' : 'A propos'}
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   )
