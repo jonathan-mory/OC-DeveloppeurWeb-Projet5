1f76573ef5d3cde85f405a17bfb3aaa426581e5d
Jonathan Mory <jomory@orange.fr>
2025-02-03 17:53:29 +0100

    Création du composant Slideshow et de son style css

---
+++ src/components/Slideshow/index.jsx
@@ -0,0 +1,53 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import './Slideshow.scss'
+
+function Slideshow({ galleryImages }) {
+  const [index, setIndex] = useState(0)
+
+  const goToPrevious = () => {
+    setIndex((currentIndex) =>
+      currentIndex === 0 ? galleryImages.length - 1 : currentIndex - 1
+    )
+  }
+
+  const goToNext = () => {
+    setIndex((currentIndex) =>
+      currentIndex === galleryImages.length - 1 ? 0 : currentIndex + 1
+    )
+  }
+
+  return (
+    <div className="slideshow">
+      <button
+        className="slideshow__button slideshow__button--prev"
+        onClick={goToPrevious}
+      >
+        <FontAwesomeIcon icon={faChevronLeft} className="slideshow__icon" />
+      </button>
+      <img
+        className="slideshow__image"
+        src={galleryImages[index]}
+        alt={`Slide ${index + 1}`}
+      />
+      <span className="slideshow__indicator">{`${index + 1}/${
+        galleryImages.length
+      }`}</span>
+      <button
+        className="slideshow__button slideshow__button--next"
+        onClick={goToNext}
+      >
+        <FontAwesomeIcon icon={faChevronRight} className="slideshow__icon" />
+      </button>
+    </div>
+  )
+}
+
+Slideshow.propTypes = {
+  galleryImages: PropTypes.array.isRequired,
+}
+
+export default Slideshow
