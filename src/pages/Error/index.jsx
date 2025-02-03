b12bee364d8d56b972a04b432c4e2512bf68732f
Jonathan Mory <jomory@orange.fr>
2025-02-03 16:33:43 +0100

    Modification de la page Error et création de son style css

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router'
+import './Error.scss'
+
 function Error() {
-  return <h1>Erreur 404</h1>
+  return (
+    <div className="error-container">
+      <span className="error-container__title">404</span>
+      <span className="error-container__description">
+        Oups ! La page que vous demandez n'existe pas.
+      </span>
+      <Link to="/" className="error-container__link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  )
 }
 
 export default Error
