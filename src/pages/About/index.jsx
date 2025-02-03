29d796f1d64715f52a42485961ff54393ac6b9d1
Jonathan Mory <jomory@orange.fr>
2025-02-03 16:32:44 +0100

    Modification de la page About et création du style CSS

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,46 @@
+import Banner from '../../components/Banner'
+import AboutBanner from '../../assets/images/about_banner.jpg'
+import Collapse from '../../components/Collapse'
+import './About.scss'
+
 function About() {
-  return <h1>A propos de nous</h1>
+  const aboutList = [
+    {
+      title: 'Fiabilité',
+      content:
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+      title: 'Respect',
+      content:
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+      title: 'Service',
+      content:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: 'Sécurité',
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]
+
+  return (
+    <main className="about">
+      <Banner imageUrl={AboutBanner} />
+      <div className="collapse-container">
+        {aboutList.map((element, index) => (
+          <Collapse
+            key={`${element.title} - ${index}`}
+            title={element.title}
+            content={element.content}
+          />
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default About
